9f2291b1de9b3cb39f3a0cd2f16644582f98f64e
Andrew Stine <astine@pcmallgov.com>
2011-03-18 11:23:51 -0400

    modified:   src/swank_client/core.clj
    Minimal functionality:
    -can evaluate expressions
    -prints stack traces and breaks to toplevel automatically
    -keep track of and displays current package
    -distinguishes between stream output and expression output

--- src/swank_client/core.clj
+++ src/swank_client/core.clj
@@ -6,25 +6,52 @@
 (declare dispatch-user-input)
 (declare dispatch-event)
 
-(def *server* {:name "localhost" :port 4005})
-(def *connection* nil)
+(def ^{:doc "Address and port of swank server"} *server* {:name "localhost" :port 4005})
+(def ^{:doc "Input and output stream connected to the server"} *connection* nil)
 
-(def current-thread :repl-thread)
-(def current-package "user")
-(def current-continuation 0)
+(def ^{:doc "Current server-side thread"} current-thread :repl-thread)
+(def ^{:doc "Current server-side package"} current-package (atom "user"))
+(def ^{:doc "Current RPC-call"} current-continuation (ref 0))
+(def ^{:doc "RPC-calls which haven't been returned."} unreturned-continuations (ref nil))
 
-(def event-queue (ref nil))
+(def ^{:doc "Queue of unhandled events"} event-queue (ref nil))
 
-(defmacro prog1 [& forms]
+(defmacro prog1
+  "Excecutes a sequence of forms, returning the value of the _first_ form executed."
+  [& forms]
   `(let [return# ~(first forms)]
      ~@(rest forms)
      return#))
 
-(defn code-message-length [length]
+(defn next-continuation
+  "Increments the continuation counter and adds it to the continuation stack.
+   Returns the new continuation."
+  []
+  (dosync
+   (alter current-continuation inc)
+   (ref-set unreturned-continuations
+	    (cons @current-continuation
+		  @unreturned-continuations)))
+  @current-continuation)
+
+(defn clear-continuation
+  "Clears a continuation from the continuation stack, indicating that swan
+   has returned that evaluation."
+  ([]
+     (dosync (ref-set unreturned-continuations
+		      (rest @unreturned-continuations))))
+  ([continuation]
+     (dosync (ref-set unreturned-continuations
+		      (remove #(= % continuation) @unreturned-continuations)))))
+
+(defn code-message-length
+  "Create the hex-stream which contains the message length."
+  [length]
   (let [hex (Integer/toHexString length)]
     (str (apply str (repeat (- 6 (count hex)) "0")) hex)))
 
 (defn send-sexp
+  "Sends a s-exp based message to swank."
   [connection sexp]
   (let [msg (str sexp "\n")
 	full-message (str (code-message-length (count msg)) msg)]
@@ -32,20 +59,29 @@
       (.write full-message 0 (count full-message))
       (.flush))))
 
-(defn next-continuation []
-  (inc current-continuation))
 
-(defn make-form [command & args]
+(defn make-form
+  "Constructs an rpc s-exp for sending to swank."
+  [command & args]
   (list :emacs-rex (cons command args)
-	current-package
+	@current-package
 	current-thread
 	(next-continuation)))
 
-(defn slime-rex [connection command & args]
+(defn slime-rex
+  "Send an rpc call to swank."
+  [connection command & args]
   (send-sexp connection (apply make-form (cons command args))))
 
-(defn eval-form [connection form]
+(defn eval-form
+  "Sends a form to the server to be evaluated in the lisp environment."
+  [connection form]
   (slime-rex connection 'swank:interactive-eval (str form)))
+
+(defn eval-repl-form
+  "Sends a form to the server to be evaluated in the lisp environment."
+  [connection form]
+  (slime-rex connection 'swank:listener-eval (str form)))
 
 (defn connect
   "Connect to server and return connection as a map
@@ -57,29 +93,35 @@
     (-> #(connection-handler conn) Thread. .start)
     conn))
 
-(defn read-chars [number reader]
+(defn read-chars
+  "Reads a set number of characters from an input stream."
+  [number reader]
   (let [ret-array (char-array number)]
     (.read reader ret-array 0 number)
     (apply str ret-array)))
 
-(defn read-sexp [connection]
+(defn read-sexp
+  "Reads a single s-exp message from the connection."
+  [connection]
   (let [in (:in @connection)
-	msg-length (Integer/parseInt (read-chars 6 in) 16)]
+					;each message is prefixed with a 6 char hex value
+					;dictating the length of the message
+	msg-length (Integer/parseInt (read-chars 6 in) 16)] 
     (binding [*read-eval* false]
       (read-string (read-chars msg-length in)))))
 
-(defn print-debug-trace [debug-trace]
-  (do (println (first (nth debug-trace 3)))
-      (doseq [trace-level (nth debug-trace 5)]
-	(println (second trace-level)))))
   
-(defn connection-handler [connection]
+(defn connection-handler
+  "Listens for events and passes them on to the event-queue for handling when found."
+  [connection]
   (while
       (nil? (:exit @connection))
     (let [message (read-sexp connection)]
       (dosync (ref-set event-queue (cons message @event-queue))))))
 
-(defn debugger-restart [connection level restart-number]
+(defn debugger-restart
+  "Sends a restart to the debugger."
+  [connection level restart-number]
   (slime-rex connection 'swank:invoke-nth-restart-for-emacs level restart-number))
 
 (defn quit-debugger
@@ -89,38 +131,58 @@
 
 ;; interactive loop
 
-(defn prompt-read [prompt]
+(defn prompt-read
+  "Prompt user for input and return the input."
+  [prompt]
   (print (str prompt ": "))
   (flush)
   (read-line))
 
-(defn dispatch-user-input []
-  (let [input (prompt-read "user")]
+(defn dispatch-user-input
+  "Prompt for and handles user input."
+  []
+  (let [input (prompt-read @current-package)]
     (case input
 	  "quit" :break
 	  "(quit)" :break
 	  "" nil
-	  (eval-form *connection* (read-string input)))))
+	  (eval-repl-form *connection* (read-string input)))))
 
-(defn event-input-loop []
+(defn event-input-loop
+  "Primary program loop; schedules the handling of events and user input."
+  []
   (loop [last-loop-result nil]
     (when (not (= last-loop-result :break))
+      (loop []
+	(while (empty? @event-queue))
+	(doseq [event (reverse (dosync (prog1 @event-queue
+					      (ref-set event-queue nil))))]
+	  (dispatch-event event))
+	(if-not (empty? @unreturned-continuations) (recur)))
       (recur
-       (do
-	 (while (nil? @event-queue))
-	 (println @event-queue)
-	 (doseq [event (reverse (dosync (prog1 @event-queue
-					       (ref-set event-queue nil))))]
-	   (dispatch-event event))
-	 (dispatch-user-input))))))
+       (dispatch-user-input)))))
 
-(defn dispatch-event [event]
+(defn print-debug-trace
+  "Prints a :debug event, providing a stacktrace."
+  [debug-trace]
+  (do (println (first (nth debug-trace 3)))
+      (doseq [trace-level (nth debug-trace 5)]
+	(println (second trace-level)))))
+
+(defn dispatch-event
+  "Handles an event; :return events are returned, :debug events are escaped from immediately."
+  [event]
   (case (first event)
-	:return (println (second (second event)))
-	:write-string (println (second event))
+	:return (do (clear-continuation))
+	:write-string (do (print (if (and (> (count event) 2)
+					  (= (nth event 2) :repl-result))
+				   "" "- ")
+				 (second event))
+			  (flush))
 	:debug (do (print-debug-trace event)
-		   (debugger-restart *connection* (nth event 2) 0)
+		   (quit-debugger *connection*)
 		   nil)
+	:new-package (swap! current-package (fn [op] (second event)))
 	:debug-activate nil
 	:debug-return nil
 	:indentation-update nil
